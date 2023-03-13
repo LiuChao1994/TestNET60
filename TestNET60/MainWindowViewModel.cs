0b2b075f3ae0b9ecdc8708c9e5395d1072aca6e5
liuchao862739233 <13487011994@163.com>
2023-03-14 00:19:04 +0800

    提交

---
+++ TestNET60/MainWindowViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows;
+
+namespace TestNET60
+{
+    internal partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        public string title = "你好，世界！";
+
+        partial void OnTitleChanged(string value)
+        {
+            
+        }
+
+
+        [RelayCommand]
+        public void TestBtn()
+        {
+            MessageBox.Show("点击了按钮。。。。");
+
+            Title = "修改了Title的值！";
+        }
+
+
+    }
+}
